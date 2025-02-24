b1e0c4dfbcc55c2aece26a0077f34cc20a078ba6
ispong <ispong@outlook.com>
2025-02-24 16:08:32 +0800

    GH-45 [Improve] 修复官网图片丢失 (#47)

--- docs/components/layout/home-header.vue
+++ docs/components/layout/home-header.vue
@@ -6,7 +6,7 @@
         <div @click="handleLogoClick" class="home-header-logo">
           <div class="logo">
             <img
-              src="https://isxcode.oss-cn-shanghai.aliyuncs.com/zhihuiyun/web-img/logo.jpg"
+              src="https://isxcode.oss-cn-shanghai.aliyuncs.com/zhihuiyun/website/web-img/logo.png"
               alt=""
             />
             <h1>至爻数据</h1>
