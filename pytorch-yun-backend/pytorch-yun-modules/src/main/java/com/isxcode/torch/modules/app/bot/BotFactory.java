e06daff84f8c8d06d9cd37beeceea57301bf98f6
ispong <ispong@outlook.com>
2025-03-06 14:54:04 +0800

    支持deepseek

---
+++ pytorch-yun-backend/pytorch-yun-modules/src/main/java/com/isxcode/torch/modules/app/bot/BotFactory.java
@@ -0,0 +1,21 @@
+package com.isxcode.torch.modules.app.bot;
+
+import com.isxcode.torch.backend.api.base.exceptions.IsxAppException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@RequiredArgsConstructor
+public class BotFactory {
+
+    private final ApplicationContext applicationContext;
+
+    public Bot getBot(String botType) {
+
+        return applicationContext.getBeansOfType(Bot.class).values().stream().filter(bot -> bot.name().equals(botType))
+            .findFirst().orElseThrow(() -> new IsxAppException("模型不支持"));
+    }
+
+}
