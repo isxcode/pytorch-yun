fc5756ed9a5959de03ac2096edb4b9e39519a2a0
ispong <ispong@outlook.com>
2025-03-07 19:05:09 +0800

    会话新建

---
+++ pytorch-yun-backend/pytorch-yun-api/src/main/java/com/isxcode/torch/api/chat/req/GetMaxChatIdReq.java
@@ -0,0 +1,17 @@
+package com.isxcode.torch.api.chat.req;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+public class GetMaxChatIdReq {
+
+    @Schema(title = "回话id", example = "123")
+    private String chatId;
+
+    @Schema(title = "应用id", example = "")
+    @NotEmpty(message = "应用id不能为空")
+    private String appId;
+}
