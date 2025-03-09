f68ec3e82b0b403535c8ba26417aa7ff933bebd1
ispong <ispong@outlook.com>
2025-03-09 13:20:19 +0800

    完成ai对话

---
+++ pytorch-yun-backend/pytorch-yun-api/src/main/java/com/isxcode/torch/api/chat/req/SendChatReq.java
@@ -0,0 +1,24 @@
+package com.isxcode.torch.api.chat.req;
+
+import com.isxcode.torch.api.chat.dto.ChatContent;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+public class SendChatReq {
+
+    @Schema(title = "回话id", example = "123")
+    private String chatId;
+
+    @Schema(title = "应用id", example = "")
+    @NotEmpty(message = "应用id不能为空")
+    private String appId;
+
+    @Schema(title = "最大indexId", example = "")
+    private Integer maxChatIndexId;
+
+    @Schema(title = "内容", example = "")
+    private ChatContent chatContent;
+}
---
+++ pytorch-yun-backend/pytorch-yun-api/src/main/java/com/isxcode/torch/api/model/constant/ModelCode.java
@@ -0,0 +1,10 @@
+package com.isxcode.torch.api.model.constant;
+
+public interface ModelCode {
+
+    String HUI_ALI_QWEN = "HUI_ALI_QWEN";
+
+    String HUI_ALI_DEEPSEEK_R1 = "HUI_ALI_DEEPSEEK_R1";
+
+    String HUI_ALI_DEEPSEEK_V3 = "HUI_ALI_DEEPSEEK_V3";
+}
