e8dff41bda901fbc56c9db5271d601a78608543d
ispong <ispong@outlook.com>
2025-04-17 18:49:03 +0800

    合并main分支

---
+++ ollama-yun-frontend/.prettierrc.js
@@ -0,0 +1,20 @@
+/*
+ * @Author: fanciNate
+ * @Date: 2023-06-05 21:57:49
+ * @LastEditTime: 2023-06-05 22:10:41
+ * @LastEditors: fanciNate
+ * @Description: In User Settings Edit
+ * @FilePath: /ollama-yun/ollama-yun-website/.prettierrc.js
+ */
+module.exports = {
+  useTabs: false,
+  tabWidth: 4,
+  singleQuote: true,
+  semi: false,
+  trailingComma: 'none',
+  arrowParens: 'always',
+  bracketSpacing: true,
+  endOfLine: 'auto',
+  printWidth: 120,
+  htmlWhitespaceSensitivity: 'ignore'
+}
