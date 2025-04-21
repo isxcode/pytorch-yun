ccbaf525b5c4deb7a98008679235bc62571d57b7
ispong <ispong@outlook.com>
2025-04-21 16:12:32 +0800

    修改模型文件上传

--- pytorch-yun-frontend/src/views/file-center/add-modal/index.vue
+++ pytorch-yun-frontend/src/views/file-center/add-modal/index.vue
@@ -59,20 +59,8 @@ const form = ref<FormInstance>()
 const callback = ref<any>()
 const typeList = ref([
   {
-    label: '作业',
-    value: 'JOB',
-  },
-  {
-    label: '函数',
-    value: 'FUNC',
-  },
-  {
-    label: '依赖',
-    value: 'LIB',
-  },
-  {
-    label: 'Excel',
-    value: 'EXCEL',
+    label: '模型文件',
+    value: 'MODEL_FILE',
   }
 ])
 const renderSence = ref<string>('new')
