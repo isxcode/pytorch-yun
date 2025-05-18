e078495dc1461af403040e674f1c656ad3a98dea
ispong <ispong@outlook.com>
2025-05-18 21:48:04 +0800

    跳转地址优化

--- docs/pages/[...slug].vue
+++ docs/pages/[...slug].vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <a style="color: #0969da" href="https://zhiqingyun.isxcode.com/">点击刷新</a>
+    <a style="color: #0969da" href="https://zhihuiyun.isxcode.com/">点击刷新</a>
   </div>
 </template>
