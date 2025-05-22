170052bdefce5c33f307b079ec757e96269e23ff
ispong <ispong@outlook.com>
2025-05-22 18:27:26 +0800

    添加应用启用禁用接口

---
+++ pytorch-yun-backend/pytorch-yun-api/src/main/java/com/isxcode/torch/api/app/req/EnableAppReq.java
@@ -0,0 +1,14 @@
+package com.isxcode.torch.api.app.req;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+public class EnableAppReq {
+
+    @Schema(title = "应用唯一id", example = "py_123456789")
+    @NotEmpty(message = "应用id不能为空")
+    private String id;
+}
